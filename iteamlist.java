3fd8bd1a7f99625e9c9b42bf4f8abbc664ab95f2
Shanto islam <157005989+Shantoislam2005@users.noreply.github.com>
2024-03-23 21:05:12 +0600

    Update

---
+++ iteamlist.java
@@ -0,0 +1,41 @@
+public class iteamlist {
+    
+    public double parking5 = 10000.0;
+    public double parking6 = 10000.0;
+    public double parking7 = 10000.0;
+    public double parking8 = 10000.0;
+    public double parking9 = 10000.0;
+    public double parking10 = 10000.0;
+    public double parking11 = 16000.0;
+    public double parking12 = 16000.0;
+    public double parking13 = 16000.0;
+    public double parking14 = 16000.0;
+    public double parking15 = 16000.0;
+    public double parking16 = 16000.0;
+    public double parking17 = 25000.0;
+    public double parking18 = 25000.0;
+    public double parking19 = 25000.0;
+    public double parking20 = 25000.0;
+    public double parking21 = 25000.0;
+    public double parking22 = 25000.0;
+    public double parking23 = 35000.0;
+    public double parking24 = 35000.0;
+    public double parking25 = 35000.0;
+    public double parking26 = 35000.0;
+    public double parking27 = 35000.0;
+    public double parking28 = 35000.0;
+    public double parking29 = 50000.0;
+    public double parking30 = 50000.0;
+    public double parking31 = 50000.0;
+    public double parking32 = 50000.0;
+    public double parking33 = 50000.0;
+    public double parking34 = 50000.0;
+    public double parking35 = 100000.0;
+    public double parking36 = 100000.0;
+    public double parking37 = 100000.0;
+    public double parking38 = 100000.0;
+    public double parking39 = 100000.0;
+    public double parking40 = 100000.0;
+    
+    
+}
